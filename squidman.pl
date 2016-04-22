224fa3b53827675d650f834051b4266ed0878039
kor_ea <evgeny@kornyushin.com>
2016-04-22 02:46:46 -0500

    added output

--- squidman.pl
+++ squidman.pl
@@ -15,24 +15,26 @@ die "Usage: perl -w squidman.pl squid-ip" unless (defined $squidmainip);
 #my $logfile = 'squidman.log';
 #open OUTPUT, ">>$logfile" or die $!;
 #STDOUT->fdopen( \*OUTPUT, 'w' ) or die $!;
-print "---STARTED at ".localtime()."\n";
+print "\n-------------------STARTED at ".localtime()."--------------\n";
 
 $dbh = DBI->connect("DBI:mysql:$dbname:$dbhost",$dbuser,$dbpass);
 $sth = $dbh->prepare("select ip,username,password,authorized_ip from squid_access where server=\'$squidmainip\'") ||
 	 die $dbh->errstr();
 $sth->execute();
 
-open(ACL,'>'.$acl) || die "--can\'t create $acl";
+open(ACL,'>'.$acl) || die "!!!can\'t create $acl";
 
 my $htpasswd_exits = 0;
 my $ipaclcnt = 0;
 while(@row = $sth -> fetchrow_array) {
 	my ($squidip, $squiduser, $squidpass, $squidauthip) = @row;
-	print "$squidip $squiduser $squidpass $squidauthip\n";
+	print "\nFound record: $squidip $squiduser $squidauthip\n";
 	if ($squiduser){
+		print "    Adding ACL for $squiduser\n";
 		print ACL "acl acl-name-$squiduser proxy_auth $squiduser\n";
 		print ACL "acl acl-ip-$squiduser localip $squidip\n";
-		print ACL "http_access allow acl-ip-$squiduser acl-name-$squiduser\n";
+		print ACL "http_access allow acl-ip-$squiduser acl-name-$squiduser\n\n";
+		print "    ";
 		if ($htpasswd_exists) {
 			system("/usr/bin/htpasswd -b $htpasswd $squiduser $squidpass");
 		}else{
@@ -42,20 +44,23 @@ while(@row = $sth -> fetchrow_array) {
 	}elsif($squidauthip){
 #		for my $authip (split/\,/,$squidauthip){
 		$squidauthip =~ s/\,/ /g;
+		print "    Adding ACL for IP addresses $squidauthip\n";
 		print ACL "acl acl-src-$ipaclcnt src $squidauthip\n";				
 		print ACL "acl acl-ip-$ipaclcnt localip $squidip\n";				
-		print ACL "http_access allow acl-src-$ipaclcnt acl-ip-$ipaclcnt\n";				
+		print ACL "http_access allow acl-src-$ipaclcnt acl-ip-$ipaclcnt\n\n";				
 		$ipaclcnt++;
 #		}
 	}
 }
-system("/usr/bin/scp $acl $htpasswd squid.conf $squidmainip:/etc/squid3/") == 0 || die "---can\'t copy config to remote";
-system("/usr/bin/ssh -t $squidmainip \'squid3 -k parse &1>/dev/null\'") == 0 || die "---error in squid config" ;
-system("/usr/bin/ssh -t $squidmainip \'squid3 -k reconfigure\'") == 0 || die "---can\'t reconfigure" ;
+print "\nCopying config to $squidmainip\...\n";
+system("/usr/bin/scp $acl $htpasswd squid.conf $squidmainip:/etc/squid3/") == 0 || die "!!!can\'t copy config to remote";
+#system("/usr/bin/ssh -t $squidmainip \'squid3 -k parse &1>/dev/null\'") == 0 || die "---error in squid config" ;
+print "\nApplying config...\n";
+system("/usr/bin/ssh -t $squidmainip \'squid3 -k reconfigure\'") == 0 || die "!!!can\'t reconfigure" ;
 
 
 $sth->finish();
 $dbh->disconnect();
 close(ACL);
-print "---FINISHED at ".localtime()."\n";
+print "-------------------FINISHED at ".localtime()."--------------\n\n";
 
