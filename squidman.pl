e6f5f742f17c7d7aec18d14b4155b6d91fdde18b
kor_ea <evgeny@kornyushin.com>
2016-04-22 14:28:42 -0500

    fixed order, added outgoing address, http_port

--- squidman.pl
+++ squidman.pl
@@ -18,7 +18,7 @@ die "Usage: perl -w squidman.pl squid-ip" unless (defined $squidmainip);
 print "\n-------------------STARTED at ".localtime()."--------------\n";
 
 $dbh = DBI->connect("DBI:mysql:$dbname:$dbhost",$dbuser,$dbpass);
-$sth = $dbh->prepare("select ip,username,password,authorized_ip from squid_access where server=\'$squidmainip\'") ||
+$sth = $dbh->prepare("select ip,username,password,authorized_ip,port from squid_access where server=\'$squidmainip\' order by authorized_ip desc") ||
 	 die $dbh->errstr();
 $sth->execute() || die $sth->errstr();
 
@@ -27,15 +27,20 @@ open(ACL,'>'.$acl) || die "!!! can\'t create $acl";
 my $htpasswd_exits = 0;
 my $ipaclcnt = 0;
 my $recordfound = 0;
+my $httpport = 3128;
 while(@row = $sth -> fetchrow_array) {
-	my ($squidip, $squiduser, $squidpass, $squidauthip) = @row;
-	print "\nFound record: $squidip $squiduser $squidauthip\n";
+	my ($squidip, $squiduser, $squidpass, $squidauthip,$squidport) = @row;
+	print "\nFound record: $squidip:$squidport $squiduser $squidauthip\n";
 	$recordfound = 1;
+	if ($squidport){
+		$httpport = $squidport;
+	}
 	if ($squiduser){
 		print "    Adding ACL for $squiduser\n";
 		print ACL "acl acl-name-$squiduser proxy_auth $squiduser\n";
 		print ACL "acl acl-ip-$squiduser localip $squidip\n";
-		print ACL "http_access allow acl-ip-$squiduser acl-name-$squiduser\n\n";
+		print ACL "http_access allow acl-ip-$squiduser acl-name-$squiduser\n";
+		print ACL "tcp_outgoing_address $squidip acl-name-$squiduser\n\n";
 		print "    ";
 		if ($htpasswd_exists) {
 			system("/usr/bin/htpasswd -b $htpasswd $squiduser $squidpass");
@@ -43,18 +48,21 @@ while(@row = $sth -> fetchrow_array) {
 			system("/usr/bin/htpasswd -cb $htpasswd $squiduser $squidpass");
 			$htpasswd_exists = 1;
 		}
+		print "\n";
 	}elsif($squidauthip){
 #		for my $authip (split/\,/,$squidauthip){
 		$squidauthip =~ s/\,/ /g;
 		print "    Adding ACL for IP addresses $squidauthip\n";
 		print ACL "acl acl-src-$ipaclcnt src $squidauthip\n";				
 		print ACL "acl acl-ip-$ipaclcnt localip $squidip\n";				
-		print ACL "http_access allow acl-src-$ipaclcnt acl-ip-$ipaclcnt\n\n";				
+		print ACL "http_access allow acl-src-$ipaclcnt acl-ip-$ipaclcnt\n";				
+		print ACL "tcp_outgoing_address $squidip acl-src-$ipaclcnt\n\n";
 		$ipaclcnt++;
 #		}
 	}
 }
-
+print "Adding http_port $httpport\n\n";					
+print ACL "http_port $httpport\n\n";					
 close(ACL);
 $sth->finish();
 $dbh->disconnect();
